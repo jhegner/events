ad0ebe1976890708088c4a653f887e5174bca8ac
Jidlafe Hegner <jidlafehegner@gmail.com>
2020-05-15 18:41:32 -0300

    Entidade responsavel

---
+++ src/main/java/br/com/jhegner/events/entity/Responsavel.java
@@ -0,0 +1,42 @@
+package br.com.jhegner.events.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import br.com.jhegner.events.enums.ETipoResponsavel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "TAB_RESPONSAVEL")
+public class Responsavel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(insertable = true, nullable = false, unique = true)
+	private Long numeroIdentificacao;
+	private String nome;
+	private String email;
+	private String telefone;
+	@Enumerated(EnumType.STRING)
+	private ETipoResponsavel tipoResponsavel;
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+}
