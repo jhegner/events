2c173d86376a885a2952ffd4aa412599b342a09b
Jidlafe Hegner <jidlafehegner@gmail.com>
2020-05-16 00:49:06 -0300

    Nova pagina de login e dashboard

--- src/main/java/br/com/jhegner/events/controller/DashboardController.java
+++ src/main/java/br/com/jhegner/events/controller/DashboardController.java
@@ -1,38 +1,21 @@
 package br.com.jhegner.events.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import br.com.jhegner.events.dto.HotelDTO;
-import br.com.jhegner.events.dto.PesquisaParticipanteDTO;
-import br.com.jhegner.events.service.ParticipanteService;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
 
-	@Autowired
-	private ParticipanteService cs;
+	@GetMapping
+	public String goDefault() {
+		return "dashboard";
+	}
 
 	@GetMapping("/participante")
-	public String goParticipante(Model model) {
-
-		// slots
-		List<String> slots = cs.pesquisaTodosGruposLocacao();
-		model.addAttribute("slots", slots);
-
-		// hoteis
-		List<HotelDTO> hoteis = cs.pesquisaTodosHoteis();
-		model.addAttribute("hoteis", hoteis);
-
-		// participantes
-		model.addAttribute("participante", null);
-		model.addAttribute("filtroDto", new PesquisaParticipanteDTO());
-
-		return "participante";
+	public String goParticipante() {
+		return "redirect:/participante";
 	}
 
 	@GetMapping("/configuracao")
